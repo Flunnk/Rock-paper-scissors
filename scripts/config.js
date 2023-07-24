04c21b09c0fdeba96fae917eb20e52dcf7e0b3e6
Ernesto <you@example.com>
2023-07-24 01:07:11 -0600

    Contador + defaultConfig (incompleto)

---
+++ scripts/config.js
@@ -0,0 +1,76 @@
+const defaultConfig = {
+  language: {
+    code: 'es',
+    html: ''
+  },
+  rounds: 0,
+  timer: 3
+};
+
+function initializeConfig() {
+  // Verificar si las configuraciones ya existen en el almacenamiento local
+  const storedConfig = JSON.parse(localStorage.getItem('config'));
+
+  // Si no existen, guardar las configuraciones predeterminadas
+  if (!storedConfig) {
+    localStorage.setItem('config', JSON.stringify(defaultConfig));
+  }
+}
+
+// Función para obtener las configuraciones actuales
+function getConfig() {
+  const storedConfig = JSON.parse(localStorage.getItem('config'));
+  return storedConfig || defaultConfig; // Si no hay configuraciones guardadas, se utilizan las predeterminadas
+}
+
+// Función para aplicar las configuraciones en la página
+function applyConfig() {
+  const config = getConfig();
+
+  // Ejemplo de cómo cambiar el idioma en la página
+  if (config.language === 'en') {
+    // Cambiar al idioma inglés
+    // Código para cambiar el contenido de la página al inglés
+  } else {
+    // Mantener el idioma predeterminado (español)
+    // Código para cambiar el contenido de la página al español o el idioma que sea predeterminado
+  }
+
+  if (config.rounds === -1) {
+    // Código para mostrar el campo de texto para ingresar el número de rondas
+  } else {
+    // Código para mostrar el campo de selección de número de rondas
+  }
+
+  if (config.timer === -1) {
+    // Código para mostrar el campo de texto para ingresar el valor del temporizador
+  } else {
+    // Código para mostrar el campo de selección de valor del temporizador
+  }
+
+  // Ejemplo de cómo utilizar el temporizador predeterminado
+  const timerValue = config.timer;
+  // Código para utilizar el valor del temporizador en la página
+}
+
+// Llamar a las funciones de inicialización y aplicación de configuraciones
+initializeConfig();
+applyConfig();
+
+// Ejemplo de cómo cambiar la configuración del idioma
+function changeLanguage(language) {
+  const config = getConfig();
+  config.language = language;
+  localStorage.setItem('config', JSON.stringify(config));
+  applyConfig(); // Aplicar los cambios en la página
+}
+
+// Ejemplo de cómo cambiar la configuración del temporizador
+function changeTimer() {
+  const config = getConfig();
+  config.timer = count;
+  localStorage.setItem('config', JSON.stringify(config));
+}
+
+console.log(getConfig());
+
--- scripts/display.js
+++ scripts/display.js
@@ -48,15 +48,15 @@ container.addEventListener('click', function(event) {
         <div class="config-tiempo">
           <div>Tiempo:</div>
           <select class="tiempo">
-            <option selected value="0" class="seleccion">3s</option>
-            <option value="1" class="seleccion">4s</option>
-            <option value="2" class="seleccion">5s</option>
-            <option value="3" class="seleccion">6s</option>
-            <option value="4" class="seleccion">7s</option>
-            <option value="5" class="seleccion">8s</option>
-            <option value="6" class="seleccion">9s</option>
-            <option value="7" class="seleccion">10s</option>
-            <option value="8" class="seleccion">Personalizado</option>
+            <option selected value="3" class="seleccion">3s</option>
+            <option value="4" class="seleccion">4s</option>
+            <option value="5" class="seleccion">5s</option>
+            <option value="6" class="seleccion">6s</option>
+            <option value="7" class="seleccion">7s</option>
+            <option value="8" class="seleccion">8s</option>
+            <option value="9" class="seleccion">9s</option>
+            <option value="10" class="seleccion">10s</option>
+            <option value="-1" class="seleccion">Personalizado</option>
           </select>
         </div>
       </div>
@@ -68,6 +68,7 @@ container.addEventListener('click', function(event) {
   }
 });
 
+// Dos jugadores
 container.addEventListener('click', function(event) {
   if (event.target.classList.contains('js-two-players-button')) {
     container.innerHTML = `
@@ -97,15 +98,15 @@ container.addEventListener('click', function(event) {
       <div class="config-tiempo">
         <div>Tiempo:</div>
         <select class="tiempo">
-          <option selected value="0" class="seleccion">3s</option>
-          <option value="1" class="seleccion">4s</option>
-          <option value="2" class="seleccion">5s</option>
-          <option value="3" class="seleccion">6s</option>
-          <option value="4" class="seleccion">7s</option>
-          <option value="5" class="seleccion">8s</option>
-          <option value="6" class="seleccion">9s</option>
-          <option value="7" class="seleccion">10s</option>
-          <option value="8" class="seleccion">Personalizado</option>
+          <option selected value="3" class="seleccion">3s</option>
+          <option value="4" class="seleccion">4s</option>
+          <option value="5" class="seleccion">5s</option>
+          <option value="6" class="seleccion">6s</option>
+          <option value="7" class="seleccion">7s</option>
+          <option value="8" class="seleccion">8s</option>
+          <option value="9" class="seleccion">9s</option>
+          <option value="10" class="seleccion">10s</option>
+          <option value="-1" class="seleccion">Personalizado</option>
         </select>
       </div>
     </div>
@@ -113,7 +114,7 @@ container.addEventListener('click', function(event) {
 
     <button class="jugar jugar-ya">Jugar</button>
 
-    `; 
+    `;
   }
 });
 
@@ -124,9 +125,34 @@ container.addEventListener('click', function(event) {
     <div class="half half-left"></div>
     <div class="half half-right"></div>
     <div class="resultados"></div>
-    
-
+    <div class="countdown"></div>
     `
+    const config = getConfig();
+    count = config.timer;
+
+    const countdownElement = document.querySelector('.countdown');
+
+    function updateCountdown(count) {
+      countdownElement.innerHTML = count;
+    }
+
+    function startCountdown() {
+      let timer = setInterval(function() {
+        if (count === 0) {
+          clearInterval(timer);
+          countdownElement.innerHTML = '';
+        }
+        updateCountdown(count);
+        count--;
+      }, 1000);
+    }
+
+    let newElement = document.createElement('div');
+    newElement.classList.add('countdown');
+
+    
+    startCountdown();
+
     setTimeout(function() {
       document.querySelector('.half-left').classList.add(`${tuClase}`);
       document.querySelector('.half-right').classList.add(`${botClase}`);
@@ -144,7 +170,11 @@ container.addEventListener('click', function(event) {
         <button class="jugar jugar-ya jugar-otra-vez">Jugar otra vez</button>
 
         `;
-    }, 3000);
+
+      if (countdownElement) {
+        countdownElement.remove();
+      }       
+    }, count * 1000 + 1000);
 
     document.body.addEventListener('keydown', (event) => {
       if (event.key === 'q') {
@@ -164,7 +194,41 @@ container.addEventListener('click', function(event) {
   }
 });
 
+container.addEventListener('change', function(event) {
+  if (event.target.classList.contains('rondas')) {
+    const rondas = document.querySelector('.rondas');
+    const div = document.querySelector('.config-rondas');
+    count = rondas.value;
+    if (count === '-1') {
+      div.innerHTML = `
+      <div>Número de rondas: </div>
+      <input type="number" class="rondas rondas-personalizado" placeholder="Rondas">
+      `;
+    } else {
+      count = rondas.value;
+    }
+    defaultConfig.rounds = count;
+    console.log(getConfig());
+  };
+});
 
+container.addEventListener('change', function(event) {
+  if (event.target.classList.contains('tiempo')) {
+    const tiempo = document.querySelector('.tiempo');
+    const div = document.querySelector('.config-tiempo');
+    count = tiempo.value;
+    if (count === '-1') {
+      div.innerHTML = `
+      <div>Tiempo:</div>
+      <input type="number" class="tiempo tiempo-personalizado" placeholder="Tiempo">
+      `;
+    } else {
+      count = tiempo.value;
+    }
+    changeTimer();
+    console.log(getConfig());
+  };
+});
 
 
 /* 
